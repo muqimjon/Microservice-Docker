aada717c092d57be39bc7a8eab709129c52cd47e
Muqimjon <muqimjon.mr@gmail.com>
2023-10-15 20:11:03 +0500

    #3 added Configuring

---
+++ src/Services/Catalog/Catalog.API/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        => Ok(new[] { new { Name = "Muqimjon", LastName = "Mamadaliyev"} });
+    }
+}
